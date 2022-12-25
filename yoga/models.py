8d9889e5d6c0b6b17c809d209cf7e989cbf1533b
Aditya srivastava <as0730728@gmail.com>
2022-12-25 21:55:53 +0530

    autocomplete

--- yoga/models.py
+++ yoga/models.py
@@ -1,24 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Yoga(models.Model):
     name = models.CharField(max_length=300)
     picture = models.ImageField(upload_to='yogaimg')
     problem = models.CharField(max_length=500)
-    
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class userinfo(models.Model):
-    username=models.CharField(max_length=80)
-    email=models.EmailField()
-    password=models.CharField(max_length=16)
-    problem=models.CharField(max_length=50)
-    
+    username = models.CharField(max_length=80)
+    email = models.EmailField()
+    password = models.CharField(max_length=16)
+    problem = models.CharField(max_length=50)
+
     def __str__(self):
         return self.username
-    
-        
-    
